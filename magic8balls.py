4bbfd183b13b3596f0215e141fea52c0d941574c
anandchhetri <chhetrianand@gmail.com>
2019-03-27 22:35:38 +0530

    Added wait time of 5 seconds between responses

--- magic8balls.py
+++ magic8balls.py
@@ -1,6 +1,7 @@
 # magic 8 ball python program
 
 import sys
+import time
 import random
 
 ans = True
@@ -14,25 +15,41 @@ while ans:
         sys.exit()
 
     elif answers == 1:
+         print ("Thinking....")
+         time.sleep(5)
          print ("It is certain")
 
     elif answers == 2:
+         print ("Thinking....")
+         time.sleep(5)
          print ("Outlook good")
 
     elif answers == 3:
+         print ("Thinking....")
+         time.sleep(5)
          print ("You may rely on it")
 
     elif answers == 4:
+         print ("Thinking....")
+         time.sleep(5)
          print ("Ask again later")
 
     elif answers == 5:
+         print ("Thinking....")
+         time.sleep(5)
          print ("Concentrate and ask again")
 
-    elif  answers == 6:
+    elif answers == 6:
+         print ("Thinking....")
+         time.sleep(5)
          print ("Reply hazy, try again")
 
     elif answers == 7:
+         print ("Thinking....")
+         time.sleep(5)
          print ("My reply is a No")
 
     elif answers == 8:
+         print ("Thinking....")
+         time.sleep(5)
          print ("My sources say No")
