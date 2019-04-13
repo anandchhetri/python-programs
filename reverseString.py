8545791cdeb1a1fddf10b60eedf68ba766c4c41d
anandchhetri <chhetrianand@gmail.com>
2019-04-13 16:57:33 +0530

    added using loop method

--- reverseString.py
+++ reverseString.py
@@ -10,3 +10,24 @@ print (s)
 print ("The reversed string (using reversed) is :", end="")
 print (reverse(s))
 
+
+print ("##########################################")
+# Using loop
+
+def reverseloop(s):
+    str = ""
+    for i in s:
+        str = i+ str
+    return str
+
+s = "string reverse using loop"
+
+print ("The origianl string is : ",end="")
+print (s)
+
+print ("The reversed string using loops is :",end ="")
+print (reverse(s))
+
+
+print ("##########################################")
+
